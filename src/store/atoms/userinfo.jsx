ee05abf1c438e03608ae98070b319389f902b70a
sahilcodes2002 <114796210+sahilcodes2002@users.noreply.github.com>
2024-06-25 20:40:53 +0530

    Update userinfo.jsx

--- src/store/atoms/userinfo.jsx
+++ src/store/atoms/userinfo.jsx
@@ -1,8 +1,20 @@
-import { atom } from "recoil";
+import { atom } from 'recoil';
+import { loadRecoilState, saveRecoilState } from '../recoilPersistence'
+
+const USER_INFO_KEY = 'userinfo';
 
 export const info = atom({
-    key:"info",
-   default:{
-    
-   } 
-}) 
+  key: 'info',
+  default: loadRecoilState(USER_INFO_KEY, {
+    name: '',
+    username: '',
+    id: ''
+  }),
+  effects_UNSTABLE: [
+    ({ onSet }) => {
+      onSet(newState => {
+        saveRecoilState(USER_INFO_KEY, newState);
+      });
+    },
+  ],
+});
