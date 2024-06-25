3bf6928d0261bb0c2491e735914f3dd65bcc355e
sahilcodes2002 <114796210+sahilcodes2002@users.noreply.github.com>
2024-06-25 20:11:18 +0530

    Update App.jsx
    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
-import {Signup} from "./pages/Signup"
-import {Signin} from "./pages/Signin"
-import { Dashboard } from './pages/Dashboard'
-import { Settings } from './pages/Settings'
-import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil"
-import { Homepage } from './pages/Home'
-function App() {
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Signup } from './pages/Signup';
+import { Signin } from './pages/Signin';
+import { Dashboard } from './pages/Dashboard';
+import { Settings } from './pages/Settings';
+import { RecoilRoot } from 'recoil';
+import { Homepage } from './pages/Home';
 
+function App() {
   return (
     <div>
       <RecoilRoot>
-      <BrowserRouter basename="/demo">
-        <Routes>
-          <Route path='/' element={<Homepage/>}></Route>
-          <Route path='/signup' element={<Signup/>}></Route>
-          <Route path='/signin' element={<Signin/>}></Route>
-          <Route path='/dashboard' element={<Dashboard/>}></Route>
-          <Route path='/settings' element={<Settings/>}></Route>
-        </Routes>
-      </BrowserRouter>
+        <HashRouter>
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/signin' element={<Signin />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/settings' element={<Settings />} />
+          </Routes>
+        </HashRouter>
       </RecoilRoot>
-        
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
