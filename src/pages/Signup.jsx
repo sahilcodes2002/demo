b0af6aaf105e019303a691488fd83de447823bcb
sahilcodes2002 <114796210+sahilcodes2002@users.noreply.github.com>
2024-06-28 02:06:21 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -30,6 +30,10 @@ export function Signup() {
             )}
             <div className="flex flex-col justify-center">
                 <div className="w-80 rounded-xl pt-3 pb-1 bg-slate-900 ">
+                    <div className="flex justify-between">
+                    <div></div>
+                    <Link to={'/'}><svg className="text-white pr-4 h-10 w-10"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg></Link>
+                </div>
                     <div className="p-4 pt-3 pb-3 bg-slate-900">
                         <Heading title={"Sign up"} />
                     </div>
