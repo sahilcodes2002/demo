23a41655b3150c4e1c80acc2b49b5a375993ba39
sahilcodes2002 <114796210+sahilcodes2002@users.noreply.github.com>
2024-06-28 01:53:59 +0530

    Update Signin.jsx

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -60,7 +60,7 @@ export function Signin() {
           <div className="p-4 pt-3 pb-3 bg-slate-900">
             <div className="flex justify-between">
               <div></div>
-              <Link to={'/'}>back</Link>
+              <Link to={'/'}><svg className="text-white pr-4 h-8 w-8"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <line x1="18" y1="6" x2="6" y2="18" />  <line x1="6" y1="6" x2="18" y2="18" /></svg></Link>
             </div>
             <Heading title={"Sign in"} />
           </div>
