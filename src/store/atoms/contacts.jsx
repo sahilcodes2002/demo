5db28470fde8ac0f9d82a9578e11eed8c88c1ec4
sahilcodes2002 <114796210+sahilcodes2002@users.noreply.github.com>
2024-06-30 07:12:40 +0530

    Create contacts.jsx

---
+++ src/store/atoms/contacts.jsx
@@ -0,0 +1,21 @@
+import { atom } from "recoil";
+
+export const contacts = atom({
+    key:"contacts",
+   default:[] 
+}) 
+
+export const allusers = atom({
+    key:"allusers",
+   default:[] 
+})
+
+
+export const userDetails = atom({
+    key:"userDetails",
+   default:{} 
+})
+
+
+
+
